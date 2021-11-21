2ad2cc8b35b9fee85998c8ef332fc2faaf2a4a3e
magata-mio <paritesh2020@gmail.com>
2021-11-21 17:59:40 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+ $(document).ready(function() {
+    $('#responsive').lightSlider({
+        item:5,
+        loop:false,
+        slideMove:2,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed: 600,
+        controls:true,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            }
+        ]
+    });  
+  });

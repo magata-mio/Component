b9d160dfa08ee68606afaefc3f00758c5b67094e
magata-mio <paritesh2020@gmail.com>
2021-11-24 20:40:09 +0545

    trending product change

--- js/main.js
+++ js/main.js
@@ -1,28 +1,66 @@
 
- $(document).ready(function() {
-    $('#responsive').lightSlider({
-        item:5,
-        loop:false,
-        slideMove:2,
+
+
+
+// Trending Product--------------------------------------------
+$(document).ready(function () {
+    $('#responsive-trending').lightSlider({
+        item: 5,
+        loop: false,
+        slideMove: 2,
         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
         speed: 600,
-        controls:true,
-        responsive : [
-            {
-                breakpoint:800,
+        controls: true,
+        responsive: [{
+                breakpoint: 800,
                 settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
+                    item: 3,
+                    slideMove: 1,
+                    slideMargin: 6,
+                }
             },
             {
-                breakpoint:480,
+                breakpoint: 480,
                 settings: {
-                    item:2,
-                    slideMove:1
-                  }
+                    item: 2,
+                    slideMove: 1
+                }
             }
         ]
-    });  
-  });
+    });
+});
+
+//  Featured Product
+$(document).ready(function () {
+    var slider = $("#featured-slider").lightSlider({
+        controls: false,
+        loop: true,
+        item: 5,
+        pager: false,
+        responsive: [{
+                breakpoint: 900,
+                settings: {
+                    item: 3,
+                    slideMove: 1,
+                    slideMargin: 6,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    item: 1,
+                    slideMove: 1,
+                    pager: true
+                }
+            }
+        ]
+    });
+
+    $('.slideControls .slidePrev').click(function () {
+        slider.goToPrevSlide();
+    });
+
+    $('.slideControls .slideNext').click(function () {
+        slider.goToNextSlide();
+    });
+});
